fc9140108b94cee1f017152456b24b47b6e051a5
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-12 17:49:43 +0100

    added fixtures&results

--- src/components/ranking/RankingSection.vue
+++ src/components/ranking/RankingSection.vue
@@ -1,15 +1,17 @@
 <script>
 import LeagueTable from './LeagueTable.vue';
+import FixturesAndResults from './FixturesAndResults.vue';
 
 export default {
     name: 'RankingSection',
 
-    components: { LeagueTable }
+    components: { LeagueTable, FixturesAndResults }
 };
 </script>
 
 <template>
     <LeagueTable />
+    <FixturesAndResults />
 </template>
 
 <style></style>
