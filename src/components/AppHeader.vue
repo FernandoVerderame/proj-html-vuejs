2c4be6a789413b5643c784f3cfd0d6d1789e119b
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-02-12 18:03:30 +0100

    added header' s images

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,6 +4,26 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+    <header>
+        <div class="container text-center">
+            <img src="../assets/img/sponsor1.png" alt="">
+            <img src="../assets/img/sponsor2.png" alt="">
+            <img src="../assets/img/logo-football.png" alt="">
+            <span><font-awesome-icon class="text-white fa-xl ms-5" :icon="['fab', 'facebook-f']" /></span>
+            <span> <font-awesome-icon class="text-white fa-xl ms-3" :icon="['fab', 'instagram']" /></span>
+            <span><font-awesome-icon class="text-white fa-xl ms-3" :icon="['fab', 'twitter']" /></span>
 
-<style></style>
+        </div>
+    </header>
+</template>
+
+<style>
+.container {
+    background-color: black;
+}
+
+header {
+    background-color: black;
+}
+</style>
