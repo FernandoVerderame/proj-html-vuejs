2a9cca424ea5c63cc6432d94b5a4ab6914a96d06
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-12 23:05:28 +0100

    added data & v-for

--- src/components/players/PlayerList.vue
+++ src/components/players/PlayerList.vue
@@ -3,6 +3,17 @@ import PlayerCard from './PlayerCard.vue';
 
 export default {
     name: 'PlayerList',
+    data: () => ({
+        players: [{
+            name: 'Adam Brown', role: 'Forwarder', shirtNumber: 7, src: 'player1.jpg'
+        }, {
+            name: 'Michael Kayn', role: 'Forwarder', shirtNumber: 9, src: 'player2.jpg'
+        }, {
+            name: 'Micheal Lee', role: 'Forwarder', shirtNumber: 11, src: 'player3.jpg'
+        }, {
+            name: 'Ethan Smith', role: 'Midfielder', shirtNumber: 15, src: 'player4.jpg'
+        }]
+    }),
     components: { PlayerCard }
 };
 </script>
@@ -13,7 +24,7 @@ export default {
         <div class="container">
             <h3 class="text-center">Players</h3>
             <div class="row">
-                <div class="col col-3 d-flex align-items-center">
+                <div class="col col-3 d-flex align-items-center" v-for="(player, i) in players" :key="i">
                     <PlayerCard />
                 </div>
             </div>
