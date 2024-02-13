41787aa3ef7cbbffdd7a84bf8a8f84056e338691
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-13 13:22:16 +0100

    added footer top layout

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,7 +9,23 @@ export default {
     <footer>
 
         <!-- Footer Top -->
-        <div class="footer-top container"></div>
+        <div class="footer-top container">
+            <div class="row">
+                <div class="col">
+                    <div class="logo"></div>
+                    <div class="info"></div>
+                    <div class="social"></div>
+                </div>
+                <div class="col">
+                    <div class="news"></div>
+                    <div class="form"></div>
+                </div>
+                <div class="col">
+                    <div class="education"></div>
+                    <div class="links"></div>
+                </div>
+            </div>
+        </div>
 
         <!-- Footer Bottom -->
         <div class="footer-bottom text-center py-4">
