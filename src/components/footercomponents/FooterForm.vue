445ca2b86d0f8e4a28c6aaef22529fb6bd6d9a27
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-13 19:57:21 +0100

    added form element & form style

--- src/components/footercomponents/FooterForm.vue
+++ src/components/footercomponents/FooterForm.vue
@@ -4,6 +4,33 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <form>
+        <div class="input-group">
+            <input type="text" class="form-control">
+        </div>
+        <div class="button-group mt-2">
+            <button class="btn">Subscribe &#8594;</button>
+        </div>
+    </form>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.form-control {
+    width: 100%;
+    height: 70px;
+    border-radius: 50px;
+    border: 0;
+    padding: 0 30px;
+}
+
+.button-group {
+    button {
+        width: 100%;
+        height: 70px;
+        border-radius: 50px;
+        background-color: #fff;
+        color: #062860;
+    }
+}
+</style>
