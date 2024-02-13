bc67f837b827afb5a843cdecccba38466834785b
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-13 12:12:05 +0100

    added h2 into fixtures & results

--- src/components/ranking/FixturesAndResults.vue
+++ src/components/ranking/FixturesAndResults.vue
@@ -6,11 +6,18 @@ export default {
 
 <template>
     <div class="results border border-primary">
+        <h2>Fixtures & Results</h2>
     </div>
 </template>
 
 <style lang="scss" scoped>
 .results {
     width: 100%;
+}
+
+h2 {
+    text-align: center;
+    font-weight: 700;
+    margin-bottom: 40px;
 }
 </style>
