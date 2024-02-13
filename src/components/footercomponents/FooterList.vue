ac1c327dbf82db9af48eaa7da2096c73438ab8a1
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-13 19:27:41 +0100

    added li dinamic & li style

--- src/components/footercomponents/FooterList.vue
+++ src/components/footercomponents/FooterList.vue
@@ -1,11 +1,39 @@
 <script>
 export default {
-    name: 'footerList'
+    name: 'footerList',
+    props: { link: Object },
+    computed: {
+        upperCaseLink() {
+            return this.link.text.toUpperCase()
+        }
+    }
 }
 </script>
 
 <template>
-    <li></li>
+    <li><a :href="link.url">{{ upperCaseLink }}</a></li>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+li {
+    list-style-type: none;
+    margin: 10px 0;
+    display: flex;
+    align-items: center;
+
+    a {
+        text-decoration: none;
+        color: #fff;
+        font-size: 18px;
+    }
+}
+
+li:before {
+    content: "";
+    width: 10px;
+    height: 5px;
+    display: inline-block;
+    background-color: #fff;
+    margin-right: 10px;
+}
+</style>
