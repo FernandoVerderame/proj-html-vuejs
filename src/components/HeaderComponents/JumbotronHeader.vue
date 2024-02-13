25f1001df5d48d176fdf4115ff358f289573917f
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-02-13 13:00:45 +0100

    added array slider

--- src/components/HeaderComponents/JumbotronHeader.vue
+++ src/components/HeaderComponents/JumbotronHeader.vue
@@ -1,25 +1,78 @@
 <script>
 export default {
     name: 'JumbotronHeader',
+    data: () => ({
+        slider: [
 
+            {
+                id: 1,
+                image: 'slider1-1.jpg',
+                primaryText: 'Welcome To Football Club',
+                description: 'Live match fix types and results'
+            },
+            {
+                id: 2,
+                image: 'slider2-1.jpg',
+                primaryText: 'Football Club Sport Club',
+                description: 'Football is a head game'
+            },
+            {
+                id: 3,
+                image: 'slider3.jpg',
+                primaryText: 'Professional Football Football Club',
+                description: 'Football is the poetry of movement'
+            },
+            {
+                id: 4,
+                image: 'slider4.jpg',
+                primaryText: 'Football Club Sport Club',
+                description: 'Football is a head game'
+            },
+
+        ],
+        currentIndex: 0,
+    }),
+
+    computed: {
+        lastIndex() {
+            return this.slider.length - 1
+        }
+
+    },
+
+    methods: {
+        goNextIndex() {
+            if (this.currentIndex === this.lastIndex) this.currentIndex = 0
+            else {
+                this.currentIndex++
+            }
+        },
+
+        createImagePath(imageName) {
+            const url = new URL(`../../assets/img/${imageName}`, import.meta.url);
+            return url.href;
+        }
+    }
 }
 
 </script>
 
 <template>
     <div class="jumbotron">
-        <img class="jumbotron-img" src="../../assets/img/slider2-1.jpg" alt="">
-        <div class="jumbotron-text text-center lh-sm">
-            <p class="big-text">Football Club</p>
-            <p class="big-text">Sport Club</p>
-            <p class="fw-bold">Football is a head game</p>
-            <button>Learn More</button>
+        <figure v-for="(slide, i) in slider" :key="i" :class="{ active: i === currentIndex }">
+            <img class="jumbotron-img" :src="createImagePath(slide.image)" alt="#">
+            <div class="jumbotron-text text-center lh-sm container-jumbotron">
+                <p class="big-text">{{ slide.primaryText }}</p>
+                <p class="fw-bold">{{ slide.description }}</p>
+                <button>Learn More</button>
+                <p>{{ slide.image }}</p>
+            </div>
+        </figure>
+        <div class="round button-slider prev" role="button">
+            <FontAwesomeIcon :icon="['fas', 'chevron-left']" />
         </div>
-        <div>
-            <font-awesome-icon class="button-slider prev" :icon="['fas', 'chevron-left']" />
-        </div>
-        <div>
-            <i class="fa-solid fa-chevron-right button-slider next"></i>
+        <div class="round button-slider next" role="button" @click="goNextIndex">
+            <FontAwesomeIcon :icon="['fas', 'chevron-right']" />
         </div>
     </div>
 </template>
@@ -30,7 +83,7 @@ export default {
     height: 700px;
 
     img {
-        max-height: 100%;
+        height: 100%;
         width: 100%;
     }
 }
@@ -55,21 +108,31 @@ p {
 .button-slider {
     position: absolute;
     top: 50%;
-    font-size: 3rem;
-    border: 1px solid white;
+    font-size: 1rem;
+    color: white;
     border-radius: 50%;
-
+    border: 1px solid white;
+    padding: 10px 15px;
 }
 
 .next {
     right: 50px;
-    transform: translateY(-50%, -50%);
 }
 
 .prev {
     left: 50px;
-    transform: translateY(-50%, -50%);
+}
 
+.container-jumbotron {
+    width: 40%;
+}
 
+figure {
+    height: 100%;
+    display: none;
+}
+
+figure.active {
+    display: block;
 }
 </style>
