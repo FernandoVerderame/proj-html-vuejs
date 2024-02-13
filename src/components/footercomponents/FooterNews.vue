bc10a8715f2e0970d30d72d29920f7e94ed32e17
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-13 20:51:55 +0100

    imported FooterForm

--- src/components/footercomponents/FooterNews.vue
+++ src/components/footercomponents/FooterNews.vue
@@ -1,9 +1,24 @@
 <script>
+import FooterForm from './FooterForm.vue';
 export default {
-    name: 'FooterNews'
+    name: 'FooterNews',
+    components: { FooterForm }
 }
 </script>
 
-<template></template>
+<template>
+    <div class="col col-4">
+        <div class="news">
+            <div class="news-text">
+                <h3>Join Our Newsletter</h3>
+                <p>Subscribe to be informed about important developments in our club and football world.</p>
+            </div>
+            <div class="news-form"></div>
+        </div>
+        <div class="form">
+            <FooterForm placeholder="Your Email Addres" @submit-form="sendForm" />
+        </div>
+    </div>
+</template>
 
 <style lang="scss" scoped></style>
