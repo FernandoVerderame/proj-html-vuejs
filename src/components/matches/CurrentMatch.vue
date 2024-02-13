b7a2fa9aaf9a90b559e820b80148d50ddb538893
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-02-13 12:08:51 +0100

    currentMatch and card updated

--- src/components/matches/CurrentMatch.vue
+++ src/components/matches/CurrentMatch.vue
@@ -1,56 +1,9 @@
 <script>
-import { faShoePrints } from '@fortawesome/free-solid-svg-icons';
+import MatchCard from "./MatchCard.vue"
 
 export default {
     name: 'CurrentMatch',
     data: () => ({
-        teams: [
-            {
-                id: 1,
-                name: "Istanbul Sports",
-                logo: "league-club-1"
-            },
-            {
-                id: 2,
-                name: "United Fs Club",
-                logo: "league-club-2"
-            },
-            {
-                id: 3,
-                name: "Schalke Club",
-                logo: "league-club-3"
-            },
-            {
-                id: 4,
-                name: "Inter Club",
-                logo: "league-club-4"
-            },
-            {
-                id: 5,
-                name: "Madrid Club",
-                logo: "league-club-5"
-            },
-            {
-                id: 6,
-                name: "Schalke Club",
-                logo: "league-club-6"
-            },
-            {
-                id: 7,
-                name: "Arsenal Club",
-                logo: "league-club-7"
-            },
-            {
-                id: 8,
-                name: "Bayern Club",
-                logo: "league-club-8"
-            },
-            {
-                id: 9,
-                name: "Atletico Club",
-                logo: "league-club-9"
-            },
-        ],
         calendar: [
             {
                 id: 1,
@@ -96,62 +49,21 @@ export default {
             },
         ]
     }),
-    methods: {
-        findTeamData(id) {
-            return this.teams.find(team => team.id === id)
-        }
-    }
+    components: { MatchCard }
 };
 </script>
 
 <template>
     <!-- Match -->
-    <section class="daily-match text-center container py-3" v-for="game in calendar" :key="game.id">
-        <!-- Squadra 1 vs Squadra 2 -->
-        <div class="teams d-flex justify-content-center align-items-center gap-3">
-            <div class="team1 d-flex align-items-center gap-2">
-                {{ findTeamData(game.team1ID).name }}
-                <img :src="`src/assets/img/${findTeamData(game.team1ID).logo}.png`" :alt="findTeamData(game.team1ID).name">
-            </div>
-            <div class="fs-1">VS</div>
-            <div class="team2 d-flex align-items-center gap-2">
-                <img :src="`src/assets/img/${findTeamData(game.team2ID).logo}.png`" :alt="findTeamData(game.team2ID).name">
-                {{ findTeamData(game.team2ID).name }}
-            </div>
-        </div>
-        <!-- Data e Luogo -->
-        <div class="details d-flex justify-content-center">
-            <div class="date"><i class="fa-regular fa-calendar-days"></i> {{ game.date }}</div>
-            <div class="Stadium"><i class="fa-regular fa-circle"></i> {{ game.stadium }}</div>
-        </div>
+    <section class="daily-match text-center container py-3">
+        <MatchCard :team1ID="calendar[0].team1ID" :team2ID="calendar[0].team2ID" :date="calendar[0].date"
+            :stadium="calendar[0].stadium" />
         <!-- More details button -->
         <button>View Match Detail</button>
     </section>
-    <!-- Daily Match -->
-    <!-- <section class="daily-match text-center container py-3"> -->
-    <!-- Squadra 1 vs Squadra 2 -->
-    <!-- <div class="teams d-flex justify-content-center align-items-center gap-3">
-            <div class="team1 d-flex align-items-center">
-                {{ findTeamData(1).name }}
-                <img :src="`src/assets/img/${findTeamData(1).logo}.png`" :alt="findTeamData(1).name">
-            </div>
-            <div class="fs-1">VS</div>
-            <div class="team2 d-flex align-items-center">
-                <img :src="`src/assets/img/${findTeamData(2).logo}.png`" :alt="findTeamData(2).name">
-                {{ findTeamData(2).name }}
-            </div>
-        </div> -->
-    <!-- Data e Luogo -->
-    <!-- <div class="details d-flex justify-content-center">
-            <div class="date"><i class="fa-regular fa-calendar-days"></i> May 21, 2022 18:05 pm</div>
-            <div class="Stadium"><i class="fa-regular fa-circle"></i> Expert Stadium</div>
-        </div> -->
-    <!-- More details button -->
-    <!-- <button>View Match Detail</button>
-    </section> -->
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .teams {
     font-size: x-large;
     font-weight: 700;
@@ -171,9 +83,5 @@ button {
     border-radius: 25px;
     font-weight: 600;
     padding: 1.2rem 3.6rem;
-}
-
-.details i {
-    font-weight: 100;
 }
 </style>
---
+++ src/components/matches/MatchCard.vue
@@ -0,0 +1,87 @@
+<script>
+export default {
+    name: 'MatchCard',
+    data: () => ({
+        teams: [
+            {
+                id: 1,
+                name: "Istanbul Sports",
+                logo: "league-club-1"
+            },
+            {
+                id: 2,
+                name: "United Fs Club",
+                logo: "league-club-2"
+            },
+            {
+                id: 3,
+                name: "Schalke Club",
+                logo: "league-club-3"
+            },
+            {
+                id: 4,
+                name: "Inter Club",
+                logo: "league-club-4"
+            },
+            {
+                id: 5,
+                name: "Madrid Club",
+                logo: "league-club-5"
+            },
+            {
+                id: 6,
+                name: "Schalke Club",
+                logo: "league-club-6"
+            },
+            {
+                id: 7,
+                name: "Arsenal Club",
+                logo: "league-club-7"
+            },
+            {
+                id: 8,
+                name: "Bayern Club",
+                logo: "league-club-8"
+            },
+            {
+                id: 9,
+                name: "Atletico Club",
+                logo: "league-club-9"
+            },
+        ],
+    }),
+    methods: {
+        findTeamData(id) {
+            return this.teams.find(team => team.id == id)
+        }
+    },
+    props: {
+        team1ID: Number,
+        team2ID: Number,
+        date: String,
+        stadium: String,
+    }
+};
+</script>
+
+<template>
+    <!-- Squadra 1 vs Squadra 2 -->
+    <div class="teams d-flex justify-content-center align-items-center gap-3">
+        <div class="team1 d-flex align-items-center gap-2">
+            {{ findTeamData(team1ID).name }}
+            <img :src="`src/assets/img/${findTeamData(team1ID).logo}.png`" :alt="findTeamData(team1ID).name">
+        </div>
+        <div class="fs-1">VS</div>
+        <div class="team2 d-flex align-items-center gap-2">
+            <img :src="`src/assets/img/${findTeamData(team2ID).logo}.png`" :alt="team2ID">
+            {{ findTeamData(team2ID).name }}
+        </div>
+    </div>
+    <!-- Data e Luogo -->
+    <div class="details d-flex justify-content-center">
+        <div class="date">{{ date }}</div>
+        <div class="Stadium">{{ stadium }}</div>
+    </div>
+</template>
+
+<!-- <style lang="scss" scoped></style> -->
