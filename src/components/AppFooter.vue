224828b45af50d9c28d246d15626e0f10faf2472
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-13 13:19:08 +0100

    added footer bottom

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,6 +4,23 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+    <!-- Footer -->
+    <footer>
 
-<style></style>
+        <!-- Footer Top -->
+        <div class="footer-top container"></div>
+
+        <!-- Footer Bottom -->
+        <div class="footer-bottom text-center py-4">
+            <p class="mb-0">2022 Football Club - All Rights Reserved.</p>
+        </div>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+    background-image: url('../assets/img/footer-bg-football.jpg');
+    color: #fff;
+}
+</style>
