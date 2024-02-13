2fda8d13756e94ba3e273d6f81b965722ec4d92d
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-13 10:40:21 +0100

    added thead table

--- src/components/ranking/LeagueTable.vue
+++ src/components/ranking/LeagueTable.vue
@@ -7,6 +7,20 @@ export default {
 <template>
     <div class="league-table border border-primary">
         <h2>League Table</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th class="team">Team</th>
+                    <th>w</th>
+                    <th>d</th>
+                    <th>l</th>
+                    <th>pts</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -18,5 +32,20 @@ export default {
 h2 {
     text-align: center;
     font-weight: 700;
+}
+
+thead {
+    tr th {
+        text-transform: uppercase;
+        text-align: center;
+        background-color: black;
+        color: white;
+        font-size: 20px;
+    }
+}
+
+.team {
+    text-align: start;
+    width: 350px;
 }
 </style>
