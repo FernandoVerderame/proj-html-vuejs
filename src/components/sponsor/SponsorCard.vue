ec484d6c41791ad2bf6e4b1b461d71899ca45e62
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-13 19:34:26 +0100

    added sponsor card structure

---
+++ src/components/sponsor/SponsorCard.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+    name: 'SponsorCard',
+
+    props: {
+        sponsor: Object
+    },
+
+    computed: {
+        sponsorImage() {
+            const url = new URL(`../../assets/img/${this.sponsor.image}`, import.meta.url);
+            return url.href;
+        }
+    }
+}
+</script>
+
+<template>
+    <!-- Sponsor card -->
+    <div class="sponsor-card col-2">
+        <figure>
+            <img :src="sponsorImage" :alt="sponsor.name">
+        </figure>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../../assets/scss/vars' as *;
+
+.sponsor-card {
+
+    figure {
+        margin: 0;
+    }
+
+    img {
+        width: 190px;
+        border: 2px solid $white;
+        padding: 12px;
+        border-radius: 30px;
+
+        &:hover {
+            transition: all 0.3s ease-in-out 0s;
+        }
+    }
+}
+</style>
