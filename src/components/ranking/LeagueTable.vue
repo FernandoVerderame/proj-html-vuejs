019cc64aef60cce50612c9df401bd5fea701f911
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-13 12:03:26 +0100

    added dinamic table league

--- src/components/ranking/LeagueTable.vue
+++ src/components/ranking/LeagueTable.vue
@@ -104,104 +104,16 @@ export default {
                 </tr>
             </thead>
             <tbody>
-                <tr>
+                <tr v-for="team in teams" :key="team.id">
                     <td class="team">
-                        <span class="ps-2">1</span>
-                        <img src="../../assets/img/league-club-1.png" alt="Instabul Sports">
-                        <span>Instabul Sports</span>
+                        <span class="ps-2">{{ team.id }}</span>
+                        <img :src="`../../assets/img/${team.logo}.png`" :alt="team.name">
+                        <span>{{ team.name }}</span>
                     </td>
-                    <td>18</td>
-                    <td>2</td>
-                    <td>0</td>
-                    <td class="score">42</td>
-                </tr>
-                <tr>
-                    <td class="team">
-                        <span class="ps-2">2</span>
-                        <img src="../../assets/img/league-club-2.png" alt="United Fs Club">
-                        <span>United Fs Club</span>
-                    </td>
-                    <td>14</td>
-                    <td>2</td>
-                    <td>1</td>
-                    <td class="score">35</td>
-                </tr>
-                <tr>
-                    <td class="team">
-                        <span class="ps-2">3</span>
-                        <img src="../../assets/img/league-club-3.png" alt="Schalke Club">
-                        <span>Schalke Club</span>
-                    </td>
-                    <td>13</td>
-                    <td>3</td>
-                    <td>0</td>
-                    <td class="score">32</td>
-                </tr>
-                <tr>
-                    <td class="team">
-                        <span class="ps-2">4</span>
-                        <img src="../../assets/img/league-club-4.png" alt="Inter Club">
-                        <span>Inter Club</span>
-                    </td>
-                    <td>13</td>
-                    <td>2</td>
-                    <td>1</td>
-                    <td class="score">31</td>
-                </tr>
-                <tr>
-                    <td class="team">
-                        <span class="ps-2">5</span>
-                        <img src="../../assets/img/league-club-5.png" alt="Madrid Club">
-                        <span>Madrid Club</span>
-                    </td>
-                    <td>12</td>
-                    <td>1</td>
-                    <td>2</td>
-                    <td class="score">30</td>
-                </tr>
-                <tr>
-                    <td class="team">
-                        <span class="ps-2">6</span>
-                        <img src="../../assets/img/league-club-6.png" alt="Schalke Club">
-                        <span>Schalke Club</span>
-                    </td>
-                    <td>11</td>
-                    <td>0</td>
-                    <td>3</td>
-                    <td class="score">28</td>
-                </tr>
-                <tr>
-                    <td class="team">
-                        <span class="ps-2">7</span>
-                        <img src="../../assets/img/league-club-7.png" alt="Arsenal Club">
-                        <span>Arsenal Club</span>
-                    </td>
-                    <td>10</td>
-                    <td>5</td>
-                    <td>0</td>
-                    <td class="score">26</td>
-                </tr>
-                <tr>
-                    <td class="team">
-                        <span class="ps-2">8</span>
-                        <img src="../../assets/img/league-club-8.png" alt="Bayern Club">
-                        <span>Bayern Club</span>
-                    </td>
-                    <td>10</td>
-                    <td>2</td>
-                    <td>2</td>
-                    <td class="score">24</td>
-                </tr>
-                <tr>
-                    <td class="team">
-                        <span class="ps-2">9</span>
-                        <img src="../../assets/img/league-club-9.png" alt="Atletico Club">
-                        <span>Atletico Club</span>
-                    </td>
-                    <td>9</td>
-                    <td>0</td>
-                    <td>4</td>
-                    <td class="score">20</td>
+                    <td>{{ team.win }}</td>
+                    <td>{{ team.draw }}</td>
+                    <td>{{ team.lose }}</td>
+                    <td class="score">{{ team.points }}</td>
                 </tr>
             </tbody>
             <tfoot>
